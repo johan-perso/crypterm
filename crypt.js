5cdeaccf8b32721eaca9777e10d3c18f2756a031
Johan <41506568+johan-perso@users.noreply.github.com>
2021-01-30 15:27:34 +0100

    Add files via upload

---
+++ crypt.js
@@ -0,0 +1,115 @@
+// Dépendances
+const Cryptr = require('cryptr'); // https://www.npmjs.com/package/cryptr
+var term = require('terminal-kit').terminal; // https://www.npmjs.com/package/terminal-kit
+const clipboardy = require('clipboardy'); // https://www.npmjs.com/package/clipboardy
+const moment = require('moment') // https://momentjs.com/
+const fs = require('fs'); // Intregré dans NodeJS
+const fetch = require('node-fetch'); // https://www.npmjs.com/package/node-fetch
+
+
+module.exports.crypt = function (text, type, key) { 
+
+    // Type de chiffrage : Classique
+    if(type === "classic"){
+        const cryptr = new Cryptr(key) // Défini la clé de chiffrage
+        const texte = cryptr.encrypt(text) // Chiffre le texte
+            .replace(/a/g, "@")
+            .replace(/b/g, "(")
+            .replace(/c/g, "#")
+            .replace(/d/g, ")");
+    
+        term("\nVotre texte est désormais chiffré : ") // Donne le texte
+        term.cyan("\n" + texte) // Donne le texte aussi
+        clipboardy.writeSync(texte); // Met le texte dans le presse papier
+        var htmlContent = "<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n<meta charset=\"utf-8\">\n<meta content=\"width=device-width, initial-scale=\"1.0\" name=\"viewport\">\n<title>Partage de message chiffré</title>\n</head>\n\n<body>\n<h1>Clé de chiffrement</h1>\n<p>" + key + "</p>\n<h1>Texte chiffré</h1>\n<p>" + texte + "</p>\n<h1>Méthode de chiffrement</h1>\n<p>aes-256-gcm-ct / classic</p>\n<h3>Déchiffrable avec <a href=\"https://github.com/johan-perso/crypterm\">Crypterm</a>.</h3>\n</body>\n\n<style>\n    h1,h2,h3,p,a {\n        font-family: Arial, Helvetica, sans-serif;\n        color: #000\n    }\n    body {\n        background-color: #4a348c;\n    }\n</style>" // Contenu du fichier HTML a crée
+        if(!fs.existsSync(__dirname + "/Partage")) fs.mkdirSync(__dirname + "/Partage") // Crée un dossier partage si il n'existe pas
+        fs.writeFile(__dirname + "/Partage/" + moment().format('YYYY.MM.DD-HH.mm.ss') + ".html", htmlContent, function (err) { // Crée un fichier de partage
+            if(err) return term.red(err) // Si il y a une erreur, La donner
+            process.exit() // Arrête le processus
+         });
+    }
+
+    // Type de chiffrage : Classique-kad
+    if(type === "classic-kad"){
+        const cryptr = new Cryptr("\x61\x65\x73\x2D\x32\x35\x36\x2D\x67\x63\x6D\x2D\x63\x74\x6B\x61\x64") // Défini la clé de chiffrage (Obfusqué)
+        const texte = cryptr.encrypt(text) // Chiffre le texte
+            .replace(/a/g, "@")
+            .replace(/b/g, "(")
+            .replace(/c/g, "#")
+            .replace(/d/g, ")");
+    
+        term("\nVotre texte est désormais chiffré : ") // Donne le texte
+        term.cyan("\n" + texte) // Donne le texte aussi
+        clipboardy.writeSync(texte); // Met le texte dans le presse papier
+        var htmlContent = "<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n<meta charset=\"utf-8\">\n<meta content=\"width=device-width, initial-scale=\"1.0\" name=\"viewport\">\n<title>Partage de message chiffré</title>\n</head>\n\n<body>\n<h1>Clé de chiffrement</h1>\n<p>" + key + "</p>\n<h1>Texte chiffré</h1>\n<p>" + texte + "</p>\n<h1>Méthode de chiffrement</h1>\n<p>aes-256-gcm-ctkad / classic-kad</p>\n<h3>Déchiffrable avec <a href=\"https://github.com/johan-perso/crypterm\">Crypterm</a>.</h3>\n</body>\n\n<style>\n    h1,h2,h3,p,a {\n        font-family: Arial, Helvetica, sans-serif;\n        color: #000\n    }\n    body {\n        background-color: #4a348c;\n    }\n</style>" // Contenu du fichier HTML a crée
+        if(!fs.existsSync(__dirname + "/Partage")) fs.mkdirSync(__dirname + "/Partage") // Crée un dossier partage si il n'existe pas
+        fs.writeFile(__dirname + "/Partage/" + moment().format('YYYY.MM.DD-HH.mm.ss') + ".html", htmlContent, function (err) { // Crée un fichier de partage
+            if(err) return term.red(err) // Si il y a une erreur, La donner
+            process.exit() // Arrête le processus
+         });
+    }
+
+    // Type de chiffrage : Binaire
+    if(type === "binary"){
+        fetch('https://no-api-key.com/api/v1/binary?text=' + text) // Fetch une API en JSON
+            .then(res => res.json())
+            .then(json => {
+                if(json.error === "missing binary query") return term.red("Veuillez entrer un texte à chiffrer.") && process.exit()
+                if(json.error === "missing text query") return term.red("Veuillez entrer un texte à chiffrer.") && process.exit()
+                if(json.error) return term.red("Une erreur inconnu s'est produite : " + json.error) && process.exit()
+                var texte = json.binary
+                term("\nVotre texte est désormais chiffré : ") // Donne le texte
+                term.cyan("\n" + texte) // Donne le texte aussi
+                clipboardy.writeSync(texte); // Met le texte dans le presse papier
+                var htmlContent = "<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n<meta charset=\"utf-8\">\n<meta content=\"width=device-width, initial-scale=\"1.0\" name=\"viewport\">\n<title>Partage de message chiffré</title>\n</head>\n\n<body>\n<h1>Texte chiffré</h1>\n<p>" + texte + "</p>\n<h1>Méthode de chiffrement</h1>\n<p>Binaire</p>\n<h3>Déchiffrable avec <a href=\"https://github.com/johan-perso/crypterm\">Crypterm</a>.</h3>\n</body>\n\n<style>\n    h1,h2,h3,p,a {\n        font-family: Arial, Helvetica, sans-serif;\n        color: #000\n    }\n    body {\n        background-color: #4a348c;\n    }\n</style>" // Contenu du fichier HTML a crée
+                if(!fs.existsSync(__dirname + "/Partage")) fs.mkdirSync(__dirname + "/Partage") // Crée un dossier partage si il n'existe pas
+                fs.writeFile(__dirname + "/Partage/" + moment().format('YYYY.MM.DD-HH.mm.ss') + ".html", htmlContent, function (err) { // Crée un fichier de partage
+                    if(err) return term.red(err) // Si il y a une erreur, La donner
+                    process.exit() // Arrête le processus
+                 });
+            })
+            .catch(err => term.red("Une erreur inconnu s'est produite.") && process.exit());
+    }
+};
+
+module.exports.uncrypt = function (text, type, key) { 
+
+    // Type de déchiffrage : Classique
+    if(type === "classic"){
+        const cryptr = new Cryptr(key); // Défini la clé de chiffrage
+
+        const texte = cryptr.decrypt(text.replace(/@/g, "a").replace(/\(/g, "b").replace(/#/g, "c").replace(/\)/g, "d")) // Déchiffre le texte
+        term("\nVotre texte est désormais déchiffré : ") // Donne le texte
+        term.cyan("\n" + texte) // Donne le texte aussi
+        clipboardy.writeSync(texte); // Met le texte dans le presse papier
+        process.exit() // Arrête le processus
+    }
+
+    // Type de déchiffrage : Classique-kad
+    if(type === "classic-kad"){
+        const cryptr = new Cryptr("\x61\x65\x73\x2D\x32\x35\x36\x2D\x67\x63\x6D\x2D\x63\x74\x6B\x61\x64") // Défini la clé de chiffrage (Obfusqué)
+        
+        const texte = cryptr.decrypt(text.replace(/@/g, "a").replace(/\(/g, "b").replace(/#/g, "c").replace(/\)/g, "d")) // Déchiffre le texte
+        term("\nVotre texte est désormais déchiffré : ") // Donne le texte
+        term.cyan("\n" + texte) // Donne le texte aussi
+        clipboardy.writeSync(texte); // Met le texte dans le presse papier
+        process.exit() // Arrête le processus
+    }
+
+    // Type de déchiffrage : Binaire
+    if(type === "binary"){
+        fetch('https://no-api-key.com/api/v1/binary-text?binary=' + text) // Fetch une API en JSON
+            .then(res => res.json())
+            .then(json => {
+                if(json.error === "missing binary query") return term.red("Veuillez entrer un texte à chiffrer.") && process.exit()
+                if(json.error === "missing text query") return term.red("Veuillez entrer un texte à chiffrer.") && process.exit()
+                if(json.error) return term.red("Une erreur inconnu s'est produite : " + json.error) && process.exit()
+                var texte = json.text.replace(/%E0/g, "à").replace(/%E9/g, "é").replace(/%E8/g, "è").replace(/%5C/g, "\\").replace(/%22/g, "\"")
+                term("\nVotre texte est désormais déchiffré : ") // Donne le texte
+                term.cyan("\n" + texte) // Donne le texte aussi
+                clipboardy.writeSync(texte); // Met le texte dans le presse papier
+                process.exit() // Arrête le processus
+            })
+            .catch(err => term.red("Une erreur inconnu s'est produite.") && process.exit());
+    }
+};
---
+++ index.js
@@ -0,0 +1,76 @@
+// Dépendances
+var term = require('terminal-kit').terminal; // https://www.npmjs.com/package/terminal-kit
+const crypterm = require('./crypt.js'); // Fichier local
+
+// Partie principale du code
+term("Que voulez vous faire ?") // Demande ce qu'on veut faire
+
+term.singleColumnMenu(["Chiffrer","Déchiffrer"], function(error, response){
+        if(response.selectedIndex === 0) var method = "crypt" // Définir METHOD avec la méthode
+        if(response.selectedIndex === 1) var method = "uncrypt" // Définir METHOD avec la méthode
+
+        term("\nQuel type de chiffrement voulez vous utilisez ?") // Demande le type de chiffrement
+        term.singleColumnMenu(["classic","classic-kad","Binaire (Bêta)"], function(error, response){
+            if(response.selectedIndex === 0) var type = "classic" // Définir TYPE avec le type de chiffrement
+            if(response.selectedIndex === 1) var type = "classic-kad" // Définir TYPE avec le type de chiffrement
+            if(response.selectedIndex === 2) var type = "binary" // Définir TYPE avec le type de chiffrement
+
+            if(response.selectedIndex === 0){
+                
+            term("\nVeuillez saisir une clé de chiffrement : ") // Demande de clé
+            term.cyan() // Met le texte écrit par l'utilisateur en cyan
+            term.inputField(function(error, key){
+                // Obligé de choisir une clé
+                if(key.length === 0){
+                    term.red("\nVeuillez saisir une clé de chiffrement.")
+                    return process.exit()
+                }   
+                // Demande une clé longue
+                if(key.length < 5){
+                    term.red("\nVeuillez saisir une clé de chiffrement plus longue.")
+                    return process.exit()
+                }  
+                // Demande de ne pas mettre de balise HTML
+                if(key.includes('<') && key.includes('/>')){
+                    term.red("\nVous ne pouvez pas entrer de code HTML.")
+                    return process.exit()
+                }
+                term("\nVeuillez saisir un texte à (dé)chiffré : ") // Demande de texte
+                term.cyan() // Met le texte écrit par l'utilisateur en cyan
+                term.inputField(function(error, text){
+                    
+                    if(method === "crypt"){ crypterm.crypt(text, type, key) } // Si la méthode est CRYPT, Dire à crypt.js de chiffrer avec les informations : text, type et key
+                    if(method === "uncrypt"){ crypterm.uncrypt(text, type, key) } // Si la méthode est UNCRYPT, Dire à crypt.js de déchiffrer avec les informations : text, type et key
+
+                }); 
+
+            });    
+        } else {
+            if(response.selectedIndex === 1 || response.selectedIndex === 2){  
+                term("\nVeuillez saisir un texte à (dé)chiffré : ") // Demande de texte
+                term.cyan() // Met le texte écrit par l'utilisateur en cyan
+                term.inputField(function(error, text){
+                    
+                    if(method === "crypt"){ crypterm.crypt(text, type) } // Si la méthode est CRYPT, Dire à crypt.js de chiffrer avec les informations : text et type
+                    if(method === "uncrypt"){ crypterm.uncrypt(text, type) } // Si la méthode est UNCRYPT, Dire à crypt.js de déchiffrer avec les informations : text et type
+
+                }); 
+            }
+        }
+    });
+        
+});
+
+// Arrêter le processus avec CTRL_Z, CTRL_C ou CTRL_D
+term.grabInput(true);
+term.on('key', function(name, matches, data){
+	if (name === 'CTRL_Z'){
+		process.exit();
+  } 
+  if (name === 'CTRL_C'){
+		process.exit();
+  }
+  if (name === 'CTRL_D'){
+		process.exit();
+	}
+  });
